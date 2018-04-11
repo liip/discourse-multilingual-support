0c7c92e6a00aeb6ea7e4bc1a46626a1f1c49c23d
Jeanmonod David <david.jeanmonod@gmail.com>
2018-04-11 20:12:12 +0200

    Add the rb manifest

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-multilingual-support
+# about: Provide fixes and tools to have a better multilingual support
+# version: 0.1
+# authors: Hugo Torres and David Jeanmonod
+# url: https://github.com/liip/discourse-multilingual-support
