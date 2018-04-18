fdd947839276b1a46b44be0b038b8512585ffaa7
Jeanmonod David <david.jeanmonod@gmail.com>
2018-04-18 17:25:28 +0200

    Dump a new version number

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-multilingual-support
 # about: Provide fixes and tools to have a better multilingual support
-# version: 0.1
+# version: 0.2
 # authors: Hugo Torres and David Jeanmonod
 # url: https://github.com/liip/discourse-multilingual-support
 
