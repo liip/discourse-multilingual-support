f6853cb682782fefe97f34cf90c18cc84523c741
Torres Hugo <hugo.torres@liip.ch>
2018-06-26 18:45:35 +0200

    issue #7 remove warnings

--- lib/single_sign_on.rb
+++ lib/single_sign_on.rb
@@ -1,10 +1,12 @@
 require_dependency 'single_sign_on'
+
+::SingleSignOn.send(:remove_const, :ACCESSORS)
+::SingleSignOn.const_set(:ACCESSORS, [
+  :nonce, :name, :username, :email, :avatar_url, :avatar_force_update, :require_activation,
+  :bio, :external_id, :return_sso_url, :admin, :moderator, :suppress_welcome_message, :title,
+  :add_groups, :remove_groups, :groups, :locale
+])
 class ::SingleSignOn
-
-  ACCESSORS = [:nonce, :name, :username, :email, :avatar_url, :avatar_force_update, :require_activation,
-               :bio, :external_id, :return_sso_url, :admin, :moderator, :suppress_welcome_message, :title,
-               :add_groups, :remove_groups, :groups, :locale]
-
   attr_accessor :locale
 
 end
